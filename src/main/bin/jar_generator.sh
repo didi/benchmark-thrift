10529267d92043e22c46abb65d83567566b58935
wangsongwangsong <wangsongwangsong@didiglobal.com>
2019-10-18 17:59:46 +0800

    评审结果修改 http://lean.xiaojukeji.com/project/ULTRON_REQ-1029/board

--- src/main/bin/jar_generator.sh
+++ src/main/bin/jar_generator.sh
@@ -21,12 +21,23 @@ function getdir(){
     done
 }
 
-
-
 declare -r HOME_DIR=$(cd $(dirname $0); cd ..; pwd)
 declare -r LIB_DIR="${HOME_DIR}/lib/thrift/$1"
 declare JAVA_FILES=""
-declare -r BASE_DIR=$(cd `dirname $0`; pwd)
+declare -r BASE_DIR=$(pwd)
+
+if [[ ! -d ${LIB_DIR} ]]; then
+    echo "Thrift版本错误"
+    exit 1
+fi
+
+
+if [[ $# < 3 ]]; then
+    echo "参数不对，第一个参数为version，第二个参数java路径，第三个参数生成的jar包位置和名称"
+    exit 1
+fi
+
+
 
 if [[ $3 == "" || $3 != *.jar ]]; then
     echo "输入正确的jar路径，确保以.jar结尾"
@@ -57,7 +68,3 @@ cd classdir
 jar -cvf $jar_path * > /dev/null
 cd ..
 rm -rf classdir
-
-
-
-
